58559b748a781aed8a39d3939039675c74c4bf4f
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-08-04 23:07:34 +0900

    .

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sine_wave as sw
-import discord_bot as dbot
-
 
 if __name__ == "__main__":
-    pass
+    import sine_wave as sw
+    import discord_bot as dbot
--- sine_wave.py
+++ sine_wave.py
@@ -4,13 +4,13 @@ import numpy as np
 
 p = pyaudio.PyAudio()
 
-volume = 0.2  # range [0.0, 1.0]
-fs = 44100  # sampling rate, Hz, must be integer
-duration = 1.0  # in seconds, may be float
-f = 440.0  # sine frequency, Hz, may be float
+# volume = 0.2  # range [0.0, 1.0]
+# fs = 44100  # sampling rate, Hz, must be integer
+# duration = 1.0  # in seconds, may be float
+# f = 440.0  # sine frequency, Hz, may be float
 
 
-def sine():
+def sine(fs, duration, f):
     # generate samples, note conversion to float32 array
     samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
 
